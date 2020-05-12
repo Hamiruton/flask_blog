d511c028f10bc2bc0e70cfc5626b20528858f821
Axel Hamilton <axelhamilton02@gmail.com>
2020-05-12 08:04:21 +0000

    Mise à jour de config.py

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,6 +1,6 @@
 class Config:
-	SECRET_KEY = '*****************************'
+	SECRET_KEY = 'd48a6a92d6a7db63d02717b32f484949'
 	SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	MAIL_USERNAME = '*********gmail.com'
-	MAIL_PASSWORD = '*********'
+	MAIL_USERNAME = 'axelhamilton02@gmail.com'
+	MAIL_PASSWORD = 'axel:hamilton:2002'
